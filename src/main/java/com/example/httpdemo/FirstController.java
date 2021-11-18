158c5f343c597e94c12a5e65c97bdf8dd71ef369
krushnatkhawale <krushnatkhawale@gmail.com>
2021-11-18 15:23:29 +0530

    Updated greeting message

--- src/main/java/com/example/httpdemo/FirstController.java
+++ src/main/java/com/example/httpdemo/FirstController.java
@@ -9,6 +9,6 @@ public class FirstController {
 
     @GetMapping({"greeting","/"})
     public String greeting(){
-        return "Hello world!";
+        return "Hello worlyfrom Spring bootd !";
     }
 }
